b944fd9d14f127593324e1b6278bc29bc1881847
darkhitman <darkhitman@c0f5e34e-b44b-11de-9b33-83dc4fd2bb11>
2009-10-29 01:32:49 +0000

    updated script, works (somewhat)

--- trunk/canopycrawler.php
+++ trunk/canopycrawler.php
@@ -1,13 +1,39 @@
 <?php
 
 class FeedCrawler {
+	public $db;
 	function run($urlTarget) {
 
+	}
+	
+	function connectToDatabase()
+	{
+	 $this->db = mysql_connect("localhost","root","");
+	}
+	
+	function submitEventDataToDatabase($name,$date,$time,$desc,$location,$source_id,$link,$tags)
+	{
+		$this->connectToDatabase();
+		$name = mysql_real_escape_string($name);
+		$date = mysql_real_escape_string($date);
+		$time = mysql_real_escape_string($time);
+		$desc = mysql_real_escape_string(strip_tags($desc));
+		$location = mysql_real_escape_string($location);
+		$datetime = strtotime($date ." ". $time);
+		
+		
+		$sql = "INSERT INTO `events`.`events_raw_events` (`source_id`, `event_time_added_at`, `event_title`, `event_description`, `event_datetime`, `event_location`, `event_link`, `event_tags`) VALUES (".$source_id.", NOW(), '".$name."', '".$desc."', FROM_UNIXTIME(".$datetime."), '.".$location.".', '".$link."', '".$tags."');";
+		
+		echo($sql);
+		mysql_query($sql,$this->db);
 	}
 	 
 }
 
 class CanopyClubCrawler extends FeedCrawler {
+
+	public $eventData;
+
 	function run($urlTarget) {
 		$file = implode(file('http://www.canopyclub.com/canopy.php'));
 		$file = str_replace("\n","",$file);
@@ -24,28 +50,36 @@ class CanopyClubCrawler extends FeedCrawler {
 			//Get the date
 			preg_match("/\<p class=\"date\"\>(.*?)\<\/p>/",$match,$date);
 			
-			print_r($date[1]." ");
+			//print_r($date[1]." ");
+			$d_date = $date[1];
 			
 			//Get the band
 			preg_match("/\<p class=\"band\"\>\<a.*?\>(.*?)\<\/a>\<\/p\>/",$match,$band);
-			print_r($band[1]. "<br/>");
+			//print_r($band[1]. "<br/>");
+			$d_band = $band[1];
 	
 			//Get the time
+			$d_time = "";
 			preg_match("/\<p class=\"info\"\>.*?(\d{1,2}\:\d\d\s?[ap]m).*?\<\/p\>/i",$match,$time);
 			if (sizeof($time) > 1)
-			print_r($time[1]. "<br/>");
+				$d_time = $time[1];
 	
 			
 			//Opening bands
 			preg_match("/\<p class=\"secband\"\>(.*?)\<\/p>/",$match,$openingbands);
+			$d_opening = "";
 			if (sizeof($openingbands) > 1)
-				print_r($openingbands[1] . "<br />") ;
+				//print_r($openingbands[1] . "<br />") ;
+				$d_opening = $openingbands[1];
 			
 			//Price
 			preg_match("/\<div class=\"caltix\"\>.*?\<p\>(.*?)\<\/p\>/",$match,$price);
+			$d_price = "";
 			if (sizeof($price) > 1)
-				print_r($price[1]. "<br/>");
+				//print_r($price[1]. "<br/>");
+				$d_price = $price[1];
 			
+			//Description
 			preg_match("/onclick\=\"NewWindow\(\'showinfo\.php\?id\=(.*?)\'/",$match,$urlShow);
 			if (sizeof($urlShow) > 1)
 			{
@@ -54,14 +88,31 @@ class CanopyClubCrawler extends FeedCrawler {
 				$file2 = str_replace("\n","",$file2);
 				
 				preg_match("/\<p class\=\"showcontent\"\>(.*?)\<\/p\>/",$file2,$desc);
-				echo($desc[1]."<br>");
+				//echo($desc[1]."<br>");
+				$d_desc = $desc[1];
 				
 			}
+			
+			//Add event to eventData.
+		global $eventData;
+		$event['name'] = $d_band . " with " . $d_opening;
+		$event['date'] = $d_date;
+		$event['time'] = $d_time;
+		$event['price'] = $d_price;
+		$event['location'] = "Canopy Club";
+		$event['desc'] = $d_desc;
+		$eventData[$key] = $event;
+		$this->submitEventDataToDB($event);
+		
 		
 		}
-		
 	
-
+		
+	}
+	
+	function submitEventDataToDB($event)
+	{
+		parent::submitEventDataToDatabase($event['name'],$event['date'],$event['time'],$event['price'].": ".$event['desc'],$event['location'],2,"www.canopyclub.com","canopy, club, music");
 	}
 
 }
