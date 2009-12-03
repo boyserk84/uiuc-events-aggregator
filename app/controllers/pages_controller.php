1bb43bf9e674de594fe2b750fcf8f012fdf862e3
darkhitman <darkhitman@c0f5e34e-b44b-11de-9b33-83dc4fd2bb11>
2009-12-03 02:02:42 +0000

    Added functionality to Pages controller for normal pages.

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -16,6 +16,36 @@ class PagesController extends AppController {
 
    }
    
+   
+ /**
+ * Displays a view
+ *
+ * @param mixed What page to display
+ * @access public
+ */
+	function display() {
+		$path = func_get_args();
+
+		$count = count($path);
+		if (!$count) {
+			$this->redirect('/');
+		}
+		$page = $subpage = $title = null;
+
+		if (!empty($path[0])) {
+			$page = $path[0];
+		}
+		if (!empty($path[1])) {
+			$subpage = $path[1];
+		}
+		if (!empty($path[$count - 1])) {
+			$title = Inflector::humanize($path[$count - 1]);
+		}
+		$this->set(compact('page', 'subpage', 'title'));
+		$this->render(join('/', $path));
+	}
+ 
+   
 }
 
 ?>
