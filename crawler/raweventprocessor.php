0502752cec0c2160502f7e92759e0e4a656050c2
darkhitman <darkhitman@c0f5e34e-b44b-11de-9b33-83dc4fd2bb11>
2010-03-12 08:26:51 +0000

    final crawlers for EOH

--- crawler/raweventprocessor.php
+++ crawler/raweventprocessor.php
@@ -1,6 +1,6 @@
 <?php
 
-define("CANOPY_CLUB", 2);
+
 /*define("ILLINI_PERFORMANCES",3);
 define("ILLINI_SPEAKERS",4);*/
 class RawEventProcessor
@@ -8,30 +8,33 @@ class RawEventProcessor
 
 	public $db;
 	
+	
 	function run($source_id)
 	{
+		echo("Running on source #".$source_id."...<br>");
 		$this->db = 0;
 		$this->connectToDatabase();
 		
 		//Clear all events with the current source id
 		$this->clear_table_of_source($source_id);
 		//Insert events with this source id into the events table, from the raw events table.
-		$this->process_events($source_id);
-		
+		$this->process_events($source_id);		
 	
 	} 
 
 	function clear_table_of_source($source_id)
 	{
-		$source_id = (int)$source_id;
+		echo("Clearing source #".$source_id."...<br>");
 		$query = "DELETE FROM events_events WHERE source_id = ".$source_id;
 		mysql_query($query,$this->db);
 	}
 	
 	function process_events($source_id)
 	{
+		echo("Processing source #".$source_id."...<br>");
 		switch ($source_id)
 		{
+			//Lots of variation here, oh yes.
 			case CANOPY_CLUB:	
 				$this->insert_data_directly_from_source($source_id);
 				break;
@@ -41,19 +44,23 @@ class RawEventProcessor
 			case ILLINI_SPEAKERS:
 				$this->insert_data_directly_from_source($source_id);
 				break;
+			case HIGHDIVE:
+				$this->insert_data_directly_from_source($source_id);
+				break;
 		
 		}
 	}
 	
 	function insert_data_directly_from_source($source_id)
 	{
+		echo("Copying over source #".$source_id."...<br>");
 		$query = "INSERT INTO events_events (source_id,event_time_added_at,"
 				. "event_title, event_description, event_datetime, event_location, "
 				. "event_link, event_tags)"
 				. " SELECT rr.source_id,rr.event_time_added_at,rr.event_title,rr.event_description,"
 				. " rr.event_datetime, rr.event_location, rr.event_link, rr.event_tags"
 				. " FROM events_raw_events AS rr WHERE rr.source_id=".$source_id;
-		echo($query);
+		echo($query."<BR>");
 		mysql_query($query,$this->db);
 	}
 
